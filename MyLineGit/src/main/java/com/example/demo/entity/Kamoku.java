ca55e098abc919c90bd9780cbce8a33dbe894a81
kagerou17 <s.nariaki107@gmail.com>
2024-02-16 18:02:27 +0900

    kamokuのentityとrepository追加

---
+++ MyLineGit/src/main/java/com/example/demo/entity/Kamoku.java
@@ -0,0 +1,252 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class Kamoku {
+
+	
+    @Id
+	
+	 @Column(name="kamoku_id")
+	 private int kamoku_id;
+
+    @Column(name="gakunen")
+    private String gakunen;
+
+    @Column(name="kamokumei")
+    private String kamokumei;
+
+    @Column(name="kamoku1")
+    private String kamoku1;
+    @Column(name="kamoku2")
+    private String kamoku2;
+    @Column(name="kamoku3")
+    private String kamoku3;
+    @Column(name="kamoku4")
+    private String kamoku4;
+    @Column(name="kamoku5")
+    private String kamoku5;
+    @Column(name="kamoku6")
+    private String kamoku6;
+    @Column(name="kamoku7")
+    private String kamoku7;
+    @Column(name="kamoku8")
+    private String kamoku8;
+    @Column(name="kamoku9")
+    private String kamoku9;
+    @Column(name="kamoku10")
+    private String kamoku10;
+    @Column(name="kamoku11")
+    private String kamoku11;
+    @Column(name="kamoku12")
+    private String kamoku12;
+    @Column(name="kamoku13")
+    private String kamoku13;
+    @Column(name="kamoku14")
+    private String kamoku14;
+
+    @Column(name="kamoku15")
+    private String kamoku15;
+
+    @Column(name="kamoku16")
+    private String kamoku16;
+
+    @Column(name="kamoku17")
+    private String kamoku17;
+
+    @Column(name="kamoku18")
+    private String kamoku18;
+
+    @Column(name="kamoku19")
+    private String kamoku19;
+
+    @Column(name="kamoku20")
+    private String kamoku20;
+
+	public int getKamoku_id() {
+		return kamoku_id;
+	}
+
+	public void setKamoku_id(int kamoku_id) {
+		this.kamoku_id = kamoku_id;
+	}
+
+	public String getGakunen() {
+		return gakunen;
+	}
+
+	public void setGakunen(String gakunen) {
+		this.gakunen = gakunen;
+	}
+
+	public String getKamokumei() {
+		return kamokumei;
+	}
+
+	public void setKamokumei(String kamokumei) {
+		this.kamokumei = kamokumei;
+	}
+
+	public String getKamoku1() {
+		return kamoku1;
+	}
+
+	public void setKamoku1(String kamoku1) {
+		this.kamoku1 = kamoku1;
+	}
+
+	public String getKamoku2() {
+		return kamoku2;
+	}
+
+	public void setKamoku2(String kamoku2) {
+		this.kamoku2 = kamoku2;
+	}
+
+	public String getKamoku3() {
+		return kamoku3;
+	}
+
+	public void setKamoku3(String kamoku3) {
+		this.kamoku3 = kamoku3;
+	}
+
+	public String getKamoku4() {
+		return kamoku4;
+	}
+
+	public void setKamoku4(String kamoku4) {
+		this.kamoku4 = kamoku4;
+	}
+
+	public String getKamoku5() {
+		return kamoku5;
+	}
+
+	public void setKamoku5(String kamoku5) {
+		this.kamoku5 = kamoku5;
+	}
+
+	public String getKamoku6() {
+		return kamoku6;
+	}
+
+	public void setKamoku6(String kamoku6) {
+		this.kamoku6 = kamoku6;
+	}
+
+	public String getKamoku7() {
+		return kamoku7;
+	}
+
+	public void setKamoku7(String kamoku7) {
+		this.kamoku7 = kamoku7;
+	}
+
+	public String getKamoku8() {
+		return kamoku8;
+	}
+
+	public void setKamoku8(String kamoku8) {
+		this.kamoku8 = kamoku8;
+	}
+
+	public String getKamoku9() {
+		return kamoku9;
+	}
+
+	public void setKamoku9(String kamoku9) {
+		this.kamoku9 = kamoku9;
+	}
+
+	public String getKamoku10() {
+		return kamoku10;
+	}
+
+	public void setKamoku10(String kamoku10) {
+		this.kamoku10 = kamoku10;
+	}
+
+	public String getKamoku11() {
+		return kamoku11;
+	}
+
+	public void setKamoku11(String kamoku11) {
+		this.kamoku11 = kamoku11;
+	}
+
+	public String getKamoku12() {
+		return kamoku12;
+	}
+
+	public void setKamoku12(String kamoku12) {
+		this.kamoku12 = kamoku12;
+	}
+
+	public String getKamoku13() {
+		return kamoku13;
+	}
+
+	public void setKamoku13(String kamoku13) {
+		this.kamoku13 = kamoku13;
+	}
+
+	public String getKamoku14() {
+		return kamoku14;
+	}
+
+	public void setKamoku14(String kamoku14) {
+		this.kamoku14 = kamoku14;
+	}
+
+	public String getKamoku15() {
+		return kamoku15;
+	}
+
+	public void setKamoku15(String kamoku15) {
+		this.kamoku15 = kamoku15;
+	}
+
+	public String getKamoku16() {
+		return kamoku16;
+	}
+
+	public void setKamoku16(String kamoku16) {
+		this.kamoku16 = kamoku16;
+	}
+
+	public String getKamoku17() {
+		return kamoku17;
+	}
+
+	public void setKamoku17(String kamoku17) {
+		this.kamoku17 = kamoku17;
+	}
+
+	public String getKamoku18() {
+		return kamoku18;
+	}
+
+	public void setKamoku18(String kamoku18) {
+		this.kamoku18 = kamoku18;
+	}
+
+	public String getKamoku19() {
+		return kamoku19;
+	}
+
+	public void setKamoku19(String kamoku19) {
+		this.kamoku19 = kamoku19;
+	}
+
+	public String getKamoku20() {
+		return kamoku20;
+	}
+
+	public void setKamoku20(String kamoku20) {
+		this.kamoku20 = kamoku20;
+	}
+    
+    
+}
